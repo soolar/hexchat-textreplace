d023237d0edaccb424a782c36d30abd89a201f45
soolar <soolar121@gmail.com>
2016-03-29 17:45:45 -0700

    protect URL and channel names, restructure data storage, add /textrep
    This is a breaking change, sorry, you'll have to remake every text replacement :(
    
    On the bright side, there's now the handy /textrep command, for when your input is only one word long, since that's the case 99% of the time. Yay!

--- textreplace.lua
+++ textreplace.lua
@@ -2,19 +2,28 @@ hexchat.register("Text Replace", 0.1, "Sets up mappings to replace certain text 
 
 local conffilename = hexchat.get_info("configdir") .. "/" .. "addon_text_replace.conf"
 
-repls = {}
+-- incredibly oversimplified URL pattern, I'll make it more complex
+-- if that turns out to be a problem.
+local urlpattern = "%S*%a%S*%.%a%S*[^%s)]"
+-- see https://tools.ietf.org/html/rfc2812#section-1.3
+local chanpattern = "#" .. string.rep("[^ \x07,]", 49, "?")
 
-local conf = io.open(conffilename,"r")
-local pat
-for line in conf:lines() do
-  if not pat then
-    pat = line
-  else
-    repls[pat] = line
-    pat = nil
+local repls = {}
+
+local delimiters = { [repls] = "\x01repl\x01" }
+
+-- load from the config file
+do
+  local conf = io.open(conffilename,"r")
+  local conftext = conf:read("*a")
+  conf:close()
+  local storepattern = {"","([^","\n]+)","([^","\n]+)","\n"}
+  for t,delimiter in pairs(delimiters) do
+    for a,b in conftext:gmatch(table.concat(storepattern,delimiter)) do
+      t[a] = b
+    end
   end
 end
-conf:close()
 
 -- creates a hook callback to be used for the given event
 -- suffixes will append to numerically matching replacements
@@ -33,13 +42,31 @@ local function makehook(event, suffixes)
 
       for i,arg in pairs(args) do
         if suffixes[i] ~= false then
-          for pat,repl in pairs(repls) do
-            arg = arg:gsub(pat,repl .. "" .. (suffixes[i] or ""))
+          local protected = {}
+          local protcount = 0
+          local function protect(text)
+            -- debug print
+            -- print("PROT\t" .. text)
+            protcount = protcount + 1
+            protected[tostring(protcount)] = text
+            return "\x00" .. protcount .. "\x00"
           end
+          -- strip out and preserve URLs and channel names
+          arg = arg:gsub(urlpattern, protect)
+          arg = arg:gsub(chanpattern, protect)
+          -- now replace any pattern matches
+          for pat,repl in pairs(repls) do
+            arg = arg:gsub(pat,repl .. "\x0f" .. (suffixes[i] or ""))
+          end
+          -- restore URLs and channel names
+          arg = arg:gsub("\x00(%d+)\x00", protected)
           args[i] = arg
         end
       end
 
+      -- re-emit the event with the modified arguments,
+      -- with this hook temporarily disabled, so that it won't
+      -- go in to a terrible infinite loop
       enabled = false
       hexchat.emit_print(event, table.unpack(args))
       enabled = true
@@ -55,30 +82,30 @@ local function addhook(event, suffixes)
   hexchat.hook_print(event, makehook(event, suffixes), hexchat.PRI_HIGHEST)
 end
 
-addhook("Channel Action", { "17", "17", false, false })
-addhook("Channel Action Hilight", { "04", "04", false, false })
-addhook("Channel Message", { "25", "", false, false })
-addhook("Channel Msg Hilight", { "04", "04", false, false })
-addhook("Channel Notice", { "30", false, "21" })
-addhook("Join", { "18", false, false })
-addhook("Notice", { "30", "21" })
-addhook("Notice Send", { "30", "21" })
-addhook("Notify Away", { "18", "24" })
-addhook("Notify Back", { "18" })
+addhook("Channel Action", { "\x0317", "\x0317", false, false })
+addhook("Channel Action Hilight", { "\x02\x0304", "\x0304", false, false })
+addhook("Channel Message", { "\x0325", "", false, false })
+addhook("Channel Msg Hilight", { "\x0304", "\x0304", false, false })
+addhook("Channel Notice", { "\x0330", false, "\x0321" })
+addhook("Join", { "\x0318", false, false })
+addhook("Notice", { "\x0330", "\x0321" })
+addhook("Notice Send", { "\x0330", "\x0321" })
+addhook("Notify Away", { "\x0318", "\x0324" })
+addhook("Notify Back", { "\x0318" })
 addhook("Notify Offline", { nil, false, false })
 addhook("Notify Online", { nil, false, false })
-addhook("Part", { "30", false, false })
-addhook("Part with Reason", { "30", false, false, nil })
-addhook("Private Action", { "18", nil, false })
-addhook("Private Action to Dialog", { "18", nil, false })
-addhook("Private Message", { "29", nil, false })
-addhook("Private Message to Dialog", { "28", nil, false })
-addhook("Quit", {"31", nil, false })
-addhook("Topic", { false, "22" })
+addhook("Part", { "\x0330", false, false })
+addhook("Part with Reason", { "\x0330", false, false, nil })
+addhook("Private Action", { "\x0318", nil, false })
+addhook("Private Action to Dialog", { "\x0318", nil, false })
+addhook("Private Message", { "\x0329", nil, false })
+addhook("Private Message to Dialog", { "\x0328", nil, false })
+addhook("Quit", {"\x0331", nil, false })
+addhook("Topic", { false, "\x0322" })
 addhook("Topic Change", { nil, nil, false })
-addhook("Topic Creation", { false, "22", false })
-addhook("Your Action", { "17", "17", false })
-addhook("Your Message", { "19", nil, false, false })
+addhook("Topic Creation", { false, "\x0322", false })
+addhook("Your Action", { "\x0317", "\x0317", false })
+addhook("Your Message", { "\x0319", nil, false, false })
 
 hexchat.hook_command("DUMPREPLS", function()
   for k,v in pairs(repls) do
@@ -88,19 +115,33 @@ hexchat.hook_command("DUMPREPLS", function()
   return hexchat.EAT_ALL
 end, "Usage: DUMPREPLS, outputs all replacements set up for Text Replace to the current window.")
 
-local function saverepls()
+local function save()
   local conf = io.open(conffilename, "w")
-  for k,v in pairs(repls) do
-    conf:write(k,"\n",v,"\n")
+  for t,delimiter in pairs(delimiters) do
+    for k,v in pairs(t) do
+      conf:write(delimiter,k,delimiter,v,delimiter,"\n")
+    end
   end
   conf:close()
 end
 
 local repin
 
+local textrepusage = "Usage: TEXTREP <input> <output>, sets input to be replaced by output whenever it shows up. Input in this case can only be one word, but output can be as many as you want. See TEXTREPINPUT and TEXTREPOUTPUT if you need a multiple word input."
+hexchat.hook_command("TEXTREP", function(words,wordeols)
+  if #words < 3 then
+    print(textrepusage)
+    return hexchat.EAT_ALL
+  end
+  print("Text Replacement created: \"" .. words[2] .. "\x0f\" -> \"" .. wordeols[3] .. "\x0f\"")
+  repls[words[2]] = wordeols[3]
+  save()
+  return hexchat.EAT_ALL
+end, textrepusage)
+
 hexchat.hook_command("TEXTREPINPUT", function(words,wordeols)
   repin = wordeols[2]
-  print("Input phrase set to \"" .. repin .. "\"")
+  print("Input phrase set to \"" .. repin .. "\x0f\"")
   return hexchat.EAT_ALL
 end, "Usage: TEXTREPINPUT <text>, sets the current input phrase for Text Replace. Use with SETTEXTREPOUT to add a new text replacement, or change an existing one.")
 
@@ -110,13 +151,13 @@ hexchat.hook_command("TEXTREPOUTPUT", function(words,wordeols)
     return hexchat.EAT_ALL
   end
   if not wordeols[2] and repls[repin] then
-    print("Text Replacement 4removed: \"" .. repin .. "\" -> \"" .. repls[repin] .. "\"")
+    print("Text Replacement \x034removed\x03: \"" .. repin .. "\x0f\" -> \"" .. repls[repin] .. "\"")
   end
   repls[repin] = wordeols[2]
   if wordeols[2] then
-    print("Text Replacement created: \"" .. repin .. "\" -> \"" .. wordeols[2] .. "\"")
+    print("Text Replacement created: \"" .. repin .. "\x0f\" -> \"" .. wordeols[2] .. "\x0f\"")
   end
-  saverepls()
+  save()
   return hexchat.EAT_ALL
 end, "Usage: TEXTREPOUTPUT <text>, sets up a replacement so that whatever text was specified by TEXTREPINPUT will be replaced with the text provided to this command. Does not work without having called TEXTREPINPUT in the first place. Call with no arguments to delete a given replacement.")
 
